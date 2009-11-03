30f7a4791eb80d4a5479ec3a8db78f7da6fdb842
Jo Geraerts <jo.geraerts@nacom.be>
2009-11-03 15:27:07 +0000

    log4jmbean

--- nascom-commons-spring/src/main/java/be/nascom/commons/spring/jmx/Log4jMbean.java
+++ nascom-commons-spring/src/main/java/be/nascom/commons/spring/jmx/Log4jMbean.java
@@ -9,25 +9,25 @@ import org.apache.log4j.Level;
  * Time: 3:32:45 PM
  */
 public class Log4jMbean {
-    public void activateInfo(String category) {
-          LogManager.getLogger(category).setLevel(Level.INFO);
-      }
 
-      public void activateDebug(String category) {
-          LogManager.getLogger(category).setLevel(Level.DEBUG);
-      }
+    public void setLevel(String category, String level){
+        LogManager.getLogger(category).setLevel(Level.toLevel(level));
+    }
 
-      public void activateWarn(String category) {
-          LogManager.getLogger(category).setLevel(Level.WARN);
-      }
+    public String getLevel(String category){
+        Level level = LogManager.getLogger(category).getLevel();
+        if(level == null){
+            return getRootLevel();
+        }
+        return level.toString();
+    }
 
-      public void activateError(String category) {
-          LogManager.getLogger(category).setLevel(Level.ERROR);
-      }
+    public void setRootLevel(String level){
+        LogManager.getRootLogger().setLevel(Level.toLevel(level));
+    }
 
-      public void activateFatal(String category) {
-          LogManager.getLogger(category).setLevel(Level.FATAL);
-      }
-    
+    public String getRootLevel(){
+        return LogManager.getRootLogger().getLevel().toString();
+    }
 
 }
