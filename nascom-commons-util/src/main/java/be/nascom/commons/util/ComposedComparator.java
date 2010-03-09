c7b32cfeee4a72a4c65c22b0b1291c88fcd4ea6e
Jo Geraerts <jo.geraerts@nacom.be>
2010-03-09 09:44:17 +0000

    added javadoc

--- nascom-commons-util/src/main/java/be/nascom/commons/util/ComposedComparator.java
+++ nascom-commons-util/src/main/java/be/nascom/commons/util/ComposedComparator.java
@@ -3,18 +3,36 @@ package be.nascom.commons.util;
 import java.util.Comparator;
 
 /**
- * User: jgeraerts
- * Date: Mar 8, 2010
- * Time: 4:55:12 PM
+ * <p>
+ * Comparator that combines the result of two other comparators. It is
+ * useful to compare multiple fields of a bean so you can sort on property a and
+ * then on property b
+ * <p>
+ * If a.compare results in nonzero it returns the value of a.compare
+ * otherwise it returns b.compare
+ *
+ * @author Jo Geraerts
+ *
  */
 public class ComposedComparator<A> implements Comparator<A> {
     private Comparator<A> a;
     private Comparator<A> b;
 
+    /**
+     *
+     * Constructor
+     *
+     * @param a Comparator a
+     * @param b Comparator b
+     */
     public ComposedComparator(Comparator<A> a, Comparator<A> b) {
         this.a = a;
         this.b = b;
     }
+
+    /**      
+     * @see java.util.Comparator#compare(Object, Object)
+     */
 
     @Override
     public int compare(A left, A right) {
--- nascom-commons-util/src/main/java/be/nascom/commons/util/ReverseOrderComparator.java
+++ nascom-commons-util/src/main/java/be/nascom/commons/util/ReverseOrderComparator.java
@@ -3,17 +3,33 @@ package be.nascom.commons.util;
 import java.util.Comparator;
 
 /**
- * User: jgeraerts
- * Date: Mar 9, 2010
- * Time: 9:59:57 AM
+ * <p>
+ * Comparator that reverses the result of an original Comparator
+ * <p> 
+ * Given Comparator a and reverseOrderComparator b
+ * <p>
+ * a.compose -> 0 -> b.compose 0 <br/>
+ * a.compose -> 1 -> b.compose -1 <br/>
+ * a.compose -> -1 -> b.compose 1 <br/>
+ *
+ * @author Jo Geraerts
+ *
  */
 public class ReverseOrderComparator<A> implements Comparator<A> {
     private Comparator<A> comparator;
 
+    /**
+     * Constructor
+     *
+     * @param comparator Comparator to reverse
+     */
     public ReverseOrderComparator(Comparator<A> comparator) {
         this.comparator = comparator;
     }
 
+    /**
+     * @see Comparator#compare(Object, Object) 
+     */
     @Override
     public int compare(A left, A right) {
         return -1 * comparator.compare(left,right);
